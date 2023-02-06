799549b952bea24f2ae3aa226d6f37bfabe30f90
hongpei100 <p1n2z3q4a5@gmail.com>
2023-02-06 21:47:47 +0800

    fix: sync slash command

--- discordBot.py
+++ discordBot.py
@@ -1,11 +1,20 @@
 from dataclasses import dataclass
 from config import Config, bot
 from botEndpoint import *
+from loguru import logger
+from model import User_info
+from db import get_session
+from loguru import logger
 
 @bot.event
 async def on_ready():
-    print("Hello!! I'm botender!!")
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} command(s)")
+    except Exception as e:
+        print(e)
+
     channel = bot.get_channel(Config.CHANNEL_ID)
-    await channel.send("Hello!! I'm botender!!")
+    await channel.send("Hola~ Bot is online now!!")
 
 bot.run(Config.BOT_TOKEN)
