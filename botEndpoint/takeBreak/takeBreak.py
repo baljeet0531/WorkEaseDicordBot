a8309e3a884df60ac7017c866ff281f7a1c8d2bb
hongpei100 <p1n2z3q4a5@gmail.com>
2023-02-06 21:45:25 +0800

    fix: Change to slash command

--- botEndpoint/takeBreak/takeBreak.py
+++ botEndpoint/takeBreak/takeBreak.py
@@ -2,6 +2,7 @@ from discord.ext import commands, tasks
 from dataclasses import dataclass
 import datetime
 import discord
+from discord import app_commands
 from config import Config
 from config import bot
 
@@ -12,30 +13,31 @@ class Session:
 
 session=  Session()
 
-@bot.command()
-async def start(ctx):
+@bot.tree.command(name = "start")
+# @app_commands.describe(interaction ="What should I say ?")
+async def start(interaction : discord.Interaction):
     if session.is_active:
-        await ctx.send("A session is already active!")
+        await interaction.response.send_message("A session is already active!")
         return
     
     session.is_active = True
-    session.start_time = ctx.message.created_at.timestamp()
-    human_readable_time = ctx.message.created_at.strftime("%H:%M:%S")
+    session.start_time = interaction.created_at.timestamp()
+    human_readable_time = interaction.created_at.strftime("%H:%M:%S")
 
     break_reminder.start()
-    await ctx.send(f"New session started at {human_readable_time}")
+    await interaction.response.send_message(f"New session started at {human_readable_time}")
 
-@bot.command()
-async def end(ctx):
+@bot.tree.command(name = "end")
+async def end(interaction : discord.Interaction):
     if not session.is_active:
-        await ctx.send("A session is already deactive!")
+        await interaction.response.send_message("A session is already deactive!")
         return
     
     session.is_active = False
-    end_time = ctx.message.created_at.timestamp()
+    end_time = interaction.created_at.timestamp()
     duration = end_time - session.start_time
     human_readable_time = str(datetime.timedelta(seconds = duration))
-    await ctx.send(f"Session last for {human_readable_time} seconds")
+    await interaction.response.send_message(f"Session last for {human_readable_time} seconds")
 
 @tasks.loop(minutes = Config.MAX_SESSION_MINUTES, count = 2)
 async def break_reminder():
