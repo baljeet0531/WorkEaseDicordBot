4ef5e2473f5a1d593715037dd271c78f685b9afd
hongpei100 <p1n2z3q4a5@gmail.com>
2023-02-06 21:40:09 +0800

    fix: add more endpoint

--- botEndpoint/__init__.py
+++ botEndpoint/__init__.py
@@ -1,3 +1,4 @@
 from botEndpoint.takeBreak.takeBreak import start, end, break_reminder
 from botEndpoint.joinExit.joinExit import on_member_join, on_member_remove
 from botEndpoint.prohibitHateSpeech.prohibitHateSpeech import on_message
+from botEndpoint.userPoints.getPoints import get_points
