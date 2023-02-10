215202762f32d3242af4c07a0619105b364eb3e4
hongpei100 <p1n2z3q4a5@gmail.com>
2023-02-11 02:54:17 +0800

    fix: fix some drink flow

--- botEndpoint/orderDrinkFlow/showAllOrder.py
+++ botEndpoint/orderDrinkFlow/showAllOrder.py
@@ -4,22 +4,25 @@ import discord
 @bot.tree.command(name="show_drink_orders")
 async def show_drink_orders(interaction: discord.Interaction):
     
-    users = Config.TODAY_ORDERS
-    # await interaction.response.send_message(users)
+    if not Config.ORDER_AVAL:
+        await interaction.response.send_message("現在尚未開始揪團, 請等飲料開團再下訂單", ephemeral = True)
+        return
 
-    # print(users.get('1068452639714578452'))
-    # print(len(users[0]['item']))
+    users = Config.TODAY_ORDERS
     num = len(users["TodayOrder"])
     print("Number", num)
-
     order = []
     for i in range(0, num):
-        name = users["TodayOrder"][i]["Name"]
-        item = users["TodayOrder"][i]["item"]
-        sugar = users["TodayOrder"][i]["Sugar"]
-        ice = users["TodayOrder"][i]["ice"]
-        order.append(name+item+sugar+ice)
-    # print("\n".join('%s' % id for id in order))
-    # print(" ".join(order)
-    # interaction.followup.send(" ".join(results.get("Name")+results.get("a1")+results.get("a2")+results.get("a3")))
-    await interaction.response.send_message("\n".join('%s' % id for id in order))
+        name = users["TodayOrder"][i]["Name"][0]
+        item = users["TodayOrder"][i]["item"][0]
+        sugar = users["TodayOrder"][i]["Sugar"][0]
+        ice = users["TodayOrder"][i]["ice"][0]
+        tmp = (name+":  "+item+" | "+sugar+" "+ice)
+        order.append(tmp)
+
+    print(order)
+
+    if any(order):
+        await interaction.response.send_message("\n".join('%s' % id for id in order), ephemeral = True)
+    else:
+        await interaction.response.send_message("現在還沒有人點任何飲料哦:beers:", ephemeral = True)
