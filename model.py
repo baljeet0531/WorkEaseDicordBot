8b289eb6f7c8d0c2518e7a55cedad991c8f4648e
hongpei100 <p1n2z3q4a5@gmail.com>
2023-02-10 12:24:19 +0800

    feat: include drink table

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, VARCHAR, BOOLEAN, ForeignKey, INTEGER, TIMESTAMP, Integer
+from sqlalchemy import Column, VARCHAR, BOOLEAN, ForeignKey, TIMESTAMP, Integer, BIGINT
 from sqlalchemy.ext.declarative import declarative_base
 
 BASE = declarative_base()
@@ -7,7 +7,7 @@ BASE = declarative_base()
 class User_info(BASE):
     __tablename__ = 'user_info'
     name = Column(VARCHAR, nullable = False)
-    user_id = Column(VARCHAR, nullable = False, primary_key = True, unique = True)
+    user_id = Column(BIGINT, nullable = False, primary_key = True, unique = True)
     points = Column(Integer, nullable = False)
     email = Column(VARCHAR, nullable = True)
 
@@ -21,8 +21,16 @@ class Emoji_info(BASE):
 
 class Event_points(BASE):
     __tablename__ = 'event_points'
-    name = Column(VARCHAR, ForeignKey("user_info.name"), nullable = False)
-    user_id = Column(VARCHAR, ForeignKey("user_info.user_id"), nullable = False, primary_key = True, unique = True)
+    name = Column(VARCHAR, nullable = False)
+    user_id = Column(BIGINT, ForeignKey("user_info.user_id"), nullable = False, primary_key = True, unique = True)
     team_buying = Column(Integer, nullable = False, default = 0)
     working_report = Column(Integer, nullable = False, default = 0)
     docs_apply = Column(Integer, nullable = False, default = 300)
+
+class Menu(BASE):
+    __tablename__ = 'menu'
+    number = Column(Integer, nullable=False, primary_key=True, unique=True)
+    brand = Column(VARCHAR, nullable=False)
+    item = Column(VARCHAR, nullable=False)
+    price_small = Column(Integer, nullable=False)
+    price_large = Column(Integer, nullable=False)
