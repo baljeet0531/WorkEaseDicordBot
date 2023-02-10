e67129d00cb1ed217108ffe057bd4e8805bf4f30
hongpei100 <p1n2z3q4a5@gmail.com>
2023-02-10 12:00:25 +0800

    feat: add more endpoints

--- botEndpoint/__init__.py
+++ botEndpoint/__init__.py
@@ -1,6 +1,7 @@
 from botEndpoint.takeBreak.takeBreak import start, end, break_reminder
-from botEndpoint.joinExit.joinExit import on_member_join, on_member_remove
-from botEndpoint.prohibitHateSpeech.prohibitHateSpeech import on_message
-from botEndpoint.userPoints.getPoints import get_points
-from botEndpoint.userPoints.queryPoints import query_points
-from botEndpoint.lottery.lottery import lottery
+from botEndpoint.joinExit import on_member_join, on_member_remove
+from botEndpoint.prohibitHateSpeech import on_message
+from botEndpoint.userPoints import get_points, query_points
+from botEndpoint.lottery import lottery
+from botEndpoint.orderDrinkFlow import see_menu, show_menu, set_deadline, show_drink_orders
+from botEndpoint.scheduleRemind import regsiter_email
