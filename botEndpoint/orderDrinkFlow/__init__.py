b578e8eaf9176cd0cc826d476ab259c03ac3bde5
hongpei100 <p1n2z3q4a5@gmail.com>
2023-02-11 02:45:02 +0800

    feat: order router

--- botEndpoint/orderDrinkFlow/__init__.py
+++ botEndpoint/orderDrinkFlow/__init__.py
@@ -1,4 +1,4 @@
-from .seeMenu import see_menu
+from .order_drinks import order_drinks
 from .setOrderDeadline import set_deadline
 from .showAllOrder import show_drink_orders
 from .showMenu import show_menu
